29bd6ac1d1a99e3896c065b0994126b6cb5bb68a
ermias-1 <vv8702in@go.minnstate.edu>
2020-02-20 03:58:18 -0600

    Add project files.

---
+++ Hydration1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Hydration1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumGlasses = new System.Windows.Forms.Label();
+            this.numGlasses = new System.Windows.Forms.NumericUpDown();
+            this.lblHydrationStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numGlasses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNumGlasses
+            // 
+            this.lblNumGlasses.AutoSize = true;
+            this.lblNumGlasses.Location = new System.Drawing.Point(194, 123);
+            this.lblNumGlasses.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNumGlasses.Name = "lblNumGlasses";
+            this.lblNumGlasses.Size = new System.Drawing.Size(346, 20);
+            this.lblNumGlasses.TabIndex = 0;
+            this.lblNumGlasses.Text = "How many glasses of water did you drink today?";
+            // 
+            // numGlasses
+            // 
+            this.numGlasses.Location = new System.Drawing.Point(272, 226);
+            this.numGlasses.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numGlasses.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numGlasses.Name = "numGlasses";
+            this.numGlasses.Size = new System.Drawing.Size(180, 26);
+            this.numGlasses.TabIndex = 1;
+            this.numGlasses.ValueChanged += new System.EventHandler(this.numGlasses_ValueChanged);
+            // 
+            // lblHydrationStatus
+            // 
+            this.lblHydrationStatus.AutoSize = true;
+            this.lblHydrationStatus.Location = new System.Drawing.Point(452, 383);
+            this.lblHydrationStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHydrationStatus.Name = "lblHydrationStatus";
+            this.lblHydrationStatus.Size = new System.Drawing.Size(0, 20);
+            this.lblHydrationStatus.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.lblHydrationStatus);
+            this.Controls.Add(this.numGlasses);
+            this.Controls.Add(this.lblNumGlasses);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Hydration";
+            ((System.ComponentModel.ISupportInitialize)(this.numGlasses)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumGlasses;
+        private System.Windows.Forms.NumericUpDown numGlasses;
+        private System.Windows.Forms.Label lblHydrationStatus;
+    }
+}
+
---
+++ Hydration1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hydration1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /* This program generate feedback or suggesstion about the hydration status
+         * of a person depending on the number of glasses of water 
+         * he/she drinks a day, and displays the result */
+        
+        private void numGlasses_ValueChanged(object sender, EventArgs e)
+        {
+            /* Get the number of glasses of water from the NumericUpDown control
+              *and generate the feedback based on the number of glasses of water
+              * the person drink a day */
+            decimal glasses = numGlasses.Value;
+
+            if (glasses >= 0 && glasses <= 1)
+            {
+
+                lblHydrationStatus.Text = "Drink some water!";
+                lblHydrationStatus.ForeColor = Color.White; // Assigning the forecolor of the labels
+                lblNumGlasses.ForeColor = Color.White;
+
+                this.BackColor = Color.Gray;      // Assigning the background color of the form
+
+
+            }
+            else if (glasses >= 2 && glasses <= 4)
+            {
+                lblHydrationStatus.Text = "Good job!";
+                lblHydrationStatus.ForeColor = Color.Black;
+                lblNumGlasses.ForeColor = Color.Black;
+
+                this.BackColor = Color.LightGray;
+
+            }
+            else if (glasses >= 5 && glasses <= 8)
+            {
+                lblHydrationStatus.Text = "Just right!";
+                lblHydrationStatus.ForeColor = Color.Black;
+                lblNumGlasses.ForeColor = Color.Black;
+
+                this.BackColor = Color.LightBlue;
+
+            }
+            else if (glasses >= 9 && glasses <= 15)
+            {
+                lblHydrationStatus.Text = "Awosome!";
+                lblHydrationStatus.ForeColor = Color.White;
+                lblNumGlasses.ForeColor = Color.White;
+
+                this.BackColor = Color.DarkBlue;
+
+            }
+            else
+            {
+                lblHydrationStatus.Text = "Too much!";
+                lblHydrationStatus.ForeColor = Color.White;
+                lblNumGlasses.ForeColor = Color.White;
+
+                this.BackColor = Color.Red;
+            }
+
+        }
+    }
+}
